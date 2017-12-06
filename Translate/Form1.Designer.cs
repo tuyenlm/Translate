564f2160c4d724add51a27916943ec9189398e48
LE MINH TUYEN <tuyen@cands-nagoya.cs-ltd.co.jp>
2017-12-06 18:14:11 +0900

    first

---
+++ Translate/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Translate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            panel1.AutoScroll = true;
+            panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            panel1.Location = new System.Drawing.Point(4, 29);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(431, 326);
+            panel1.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(184, 4);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(121, 19);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(418, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(15, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(397, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(15, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 19);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(438, 359);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transale JP-VN";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        public static System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Translate/Form1.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Translate
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+        public Form1()
+        {
+            RegistryKey add = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            add.SetValue("TranslateJP-VN", Application.StartupPath + @"\" + Process.GetCurrentProcess().ProcessName + ".exe");
+            if (add.GetValue("TranslateJP-VN") == null) add.SetValue("TranslateJP-VN", Application.StartupPath + @"\" + Process.GetCurrentProcess().ProcessName + ".exe");
+            InitializeComponent();
+            trackBar1.Value = 5;
+            trackBar1.Minimum = 3;
+            this.Opacity = trackBar1.Value / 10.0;
+            var HotKeyManager = new HotkeyManager();
+            //RegisterHotKey (Hangle, Hotkey Identifier, Modifiers, Key)
+            //RegisterHotKey(HotKeyManager.Handle, 123, Constants.ALT + Constants.SHIFT, (int)Keys.P);
+            RegisterHotKey(HotKeyManager.Handle, 234, Constants.CTRL, (int)Keys.Q);
+            this.WindowState = FormWindowState.Normal;
+        }
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+            if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
+                message.Result = (IntPtr)HTCAPTION;
+        }
+        //This class is not required but makes managing the modifiers easier.
+        public static class Constants
+        {
+            public const int NOMOD = 0x0000;
+            public const int ALT = 0x0001;
+            public const int CTRL = 0x0002;
+            public const int SHIFT = 0x0004;
+            public const int WIN = 0x0008;
+            public const int WM_HOTKEY_MSG_ID = 0x0312;
+        }
+
+        public sealed class HotkeyManager : NativeWindow, IDisposable
+        {
+            public HotkeyManager()
+            {
+                CreateHandle(new CreateParams());
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                {
+                    if (m.WParam.ToInt32() == 234)
+                    {
+                        if (Clipboard.ContainsText(TextDataFormat.Text))
+                        {
+                            string text = Clipboard.GetText(TextDataFormat.Text);
+                            loadData(text);
+                        }
+                    }
+                }
+                base.WndProc(ref m);
+            }
+
+            public static void loadData(string text)
+            {
+                panel1.Controls.Clear();
+                Label title = new Label();
+                string regExp = @"[^\w\d]";
+                string tmp = Regex.Replace(text, regExp, "");
+                string URL = "http://mazii.net/api/search/" + tmp + "/20/1";
+                using (var webClient = new WebClient { Encoding = System.Text.Encoding.UTF8 })
+                {
+                    var json = webClient.DownloadString(URL);
+                    dynamic result = JsonConvert.DeserializeObject<dynamic>(json);
+                    if (result.status == "200")
+                    {
+                        title.Text = result.data[0].word + " (" + result.data[0].phonetic + ")\n\n";
+                        title.Font = new Font("MS UI Gothic", 12, FontStyle.Bold);
+                        title.ForeColor = Color.Red;
+                        title.AutoSize = true;
+                        panel1.Controls.Add(title);
+                        int i = 1;
+                        foreach (var item in result.data[0].means)
+                        {
+                            dynamic jsonSerial = JsonConvert.SerializeObject(item);
+                            dynamic result3 = JsonConvert.DeserializeObject<dynamic>(jsonSerial);
+                            var l1 = new Label
+                            {
+                                Text = "◆ " + result3.mean + "\n\n",
+                                Font = new Font("MS UI Gothic", 12, FontStyle.Regular),
+                                Location = new Point(0, 32 * i),
+                                AutoSize = true,
+                                ForeColor = Color.Green
+                            };
+
+                            if (result3.examples != null)
+                            {
+                                dynamic jsonSerial4 = JsonConvert.SerializeObject(result3.examples);
+                                dynamic result4 = JsonConvert.DeserializeObject<dynamic>(jsonSerial4);
+                                int j = 1;
+                                foreach (var item4 in result4)
+                                {
+                                    var l2 = new Label
+                                    {
+                                        Text = "    ● " + item4.content + "\n\n       " + item4.transcription + "\n        " + item4.mean + "\n\n",
+                                        AutoSize = true,
+                                        Font = new Font("MS UI Gothic", 10, FontStyle.Regular),
+                                        Location = new Point(0, l1.Location.Y + 70 * j - 20)
+                                    };
+                                    panel1.Controls.Add(l2);
+                                    j++;
+                                }
+                            }
+                            panel1.Controls.Add(l1);
+                            i++;
+                        }
+                    }
+                    else title.Text = "Not Found!!!";
+                    panel1.Controls.Add(title);
+                }
+            }
+
+            public void Dispose()
+            {
+                DestroyHandle();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = trackBar1.Value / 10.0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("アプリケーションを終了しますか？", "閉じる", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text != "")
+                {
+                    HotkeyManager.loadData(textBox1.Text);
+                }
+            }
+        }
+    }
+}
